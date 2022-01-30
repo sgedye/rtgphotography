4a109be3ceb49a0636581c6a5150aaf6650bc4ef
Shaun Gedye <sgedye88@yahoo.com.au>
2022-01-30 18:46:38 +0800

    Web - Update Madagascar Gallery | Add images

--- src/pages/galleries/madagascar.tsx
+++ src/pages/galleries/madagascar.tsx
@@ -1,38 +1,68 @@
-import { Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
+import { graphql, Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-import { Layout, Seo } from "../../components";
+import { Layout, Loading, Seo } from "../../components";
+import type { GalleryData } from "./types";
 
-import mad01 from "../../assets/images/galleries/madagascar/Madagascar001.jpg";
 import { Routes } from "../../models/Routes";
 
-export default function Magagascar() {
+export default function Magagascar({ data }: { data: GalleryData }) {
   return (
     <Layout>
       <Seo title="Magagascar Gallery" />
       <Link className="btn btn-outline-primary mb-4" to={Routes.Galleries}>
         &larr; Back to galleries
       </Link>
-      <div className="row">
-        {loop.map(x => (
-          <div key={x} className="col-12 col-sm-6 col-lg-4 col-xl-3 mb-3">
-            {x % 3 ? (
-              <img src={mad01} alt="test" className="img-fluid" />
-            ) : (
-              <StaticImage
-                src="../images/gatsby-astronaut.png"
-                width={300}
-                quality={95}
-                formats={["auto", "webp", "avif"]}
-                alt="A Gatsby astronaut"
-                className="img-fluid"
-              />
-            )}
-          </div>
-        ))}
-      </div>
+      {(() => {
+        if (data?.galleryImages?.edges?.length) {
+          return (
+            <div className="row">
+              {data.galleryImages.edges.map(edge => {
+                const image = getImage(
+                  edge.node.childImageSharp.gatsbyImageData
+                );
+                if (image) {
+                  return (
+                    <div
+                      key={edge.node.id}
+                      className="col-12 col-md-6 col-xl-4 mb-3"
+                    >
+                      <GatsbyImage
+                        image={image}
+                        alt={edge.node.base.split(".")[0]}
+                      />
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          );
+        } else {
+          return <Loading />;
+        }
+      })()}
     </Layout>
   );
 }
 
-const loop = [1, 2, 3, 4, 5, 6, 7];
+export const pageQuery = graphql`
+  query {
+    galleryImages: allFile(
+      filter: {
+        extension: { regex: "/jpg|png|jpeg/" }
+        relativeDirectory: { eq: "galleries/madagascar" }
+      }
+      sort: { fields: base, order: ASC }
+    ) {
+      edges {
+        node {
+          id
+          base
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, JPG])
+          }
+        }
+      }
+    }
+  }
+`;
