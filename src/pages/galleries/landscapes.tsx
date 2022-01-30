b17d9ee327ee0c36bc7193fa502b3a8f251f1aaf
Shaun Gedye <sgedye88@yahoo.com.au>
2022-01-30 18:59:22 +0800

    Web - Add Landscapes Gallery | Add images

---
+++ src/pages/galleries/landscapes.tsx
@@ -0,0 +1,71 @@
+import { graphql, Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
+import { Layout, Loading, Seo } from "../../components";
+import type { GalleryData } from "./types";
+
+import { Routes } from "../../models/Routes";
+
+export default function Landscapes({ data }: { data: GalleryData }) {
+  return (
+    <Layout>
+      <Seo title="Landscapes Gallery" />
+      <Link className="btn btn-outline-primary mb-4" to={Routes.Galleries}>
+        &larr; Back to galleries
+      </Link>
+      {(() => {
+        if (data?.galleryImages?.edges?.length) {
+          return (
+            <div className="row">
+              {data.galleryImages.edges.map(edge => {
+                const image = getImage(
+                  edge.node.childImageSharp.gatsbyImageData
+                );
+                if (image) {
+                  return (
+                    <div
+                      key={edge.node.id}
+                      className="col-12 col-md-6 col-xl-4 mb-3"
+                    >
+                      <GatsbyImage
+                        image={image}
+                        alt={
+                          "Landscapes" +
+                          edge.node.base.split(".")[0].split("-")[1]
+                        }
+                      />
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          );
+        } else {
+          return <Loading />;
+        }
+      })()}
+    </Layout>
+  );
+}
+
+export const pageQuery = graphql`
+  query {
+    galleryImages: allFile(
+      filter: {
+        extension: { regex: "/jpg|png|jpeg/" }
+        relativeDirectory: { eq: "galleries/landscapes" }
+      }
+      sort: { fields: base, order: ASC }
+    ) {
+      edges {
+        node {
+          id
+          base
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, JPG])
+          }
+        }
+      }
+    }
+  }
+`;
