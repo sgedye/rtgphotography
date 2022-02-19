e3f19bdff1416981969065942c8004b81f9a8702
Shaun Gedye <sgedye88@yahoo.com.au>
2022-02-19 16:46:58 +0800

    Web - Update homepage carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
 import { cx } from "linaria";
 import { Layout, Seo } from "../components";
 
-import slide1 from "../images/galleries/madagascar/Madagascar001.jpg";
-import slide2 from "../images/galleries/madagascar/Madagascar002.jpg";
-import slide3 from "../images/galleries/madagascar/Madagascar003.jpg";
-import slide4 from "../images/galleries/madagascar/Madagascar004.jpg";
-import slide5 from "../images/galleries/madagascar/Madagascar005.jpg";
+import mobileImage1 from "../images/gfx/hero-carousel/rtgphoto-001-mobile.jpg";
+import mobileImage2 from "../images/gfx/hero-carousel/rtgphoto-002-mobile.jpg";
+import mobileImage3 from "../images/gfx/hero-carousel/rtgphoto-003-mobile.jpg";
+import mobileImage4 from "../images/gfx/hero-carousel/rtgphoto-004-mobile.jpg";
+import mobileImage5 from "../images/gfx/hero-carousel/rtgphoto-005-mobile.jpg";
+
+import desktopImage1 from "../images/gfx/hero-carousel/rtgphoto-001.jpg";
+import desktopImage2 from "../images/gfx/hero-carousel/rtgphoto-002.jpg";
+import desktopImage3 from "../images/gfx/hero-carousel/rtgphoto-003.jpg";
+import desktopImage4 from "../images/gfx/hero-carousel/rtgphoto-004.jpg";
+import desktopImage5 from "../images/gfx/hero-carousel/rtgphoto-005.jpg";
+
+import extraWideImage1 from "../images/gfx/hero-carousel/rtgphoto-001-wide.jpg";
+import extraWideImage2 from "../images/gfx/hero-carousel/rtgphoto-002-wide.jpg";
+import extraWideImage3 from "../images/gfx/hero-carousel/rtgphoto-003-wide.jpg";
+import extraWideImage4 from "../images/gfx/hero-carousel/rtgphoto-004-wide.jpg";
+import extraWideImage5 from "../images/gfx/hero-carousel/rtgphoto-005-wide.jpg";
 
 export default function Index() {
   return (
@@ -18,25 +30,37 @@ export default function Index() {
         data-bs-ride="carousel"
       >
         <div className="carousel-indicators">
-          {slides.map(({ slide, id }) => (
+          {carouselImages.map((_, idx) => (
             <button
-              key={id}
+              key={idx}
               type="button"
               data-bs-target="#carousel-homepage"
-              data-bs-slide-to={`${id}`}
-              className={id === 0 ? "active" : ""}
+              data-bs-slide-to={`${idx}`}
+              className={idx === 0 ? "active" : ""}
               aria-current="true"
-              aria-label={`Slide ${id + 1}`}
+              aria-label={`Slide ${idx + 1}`}
             ></button>
           ))}
         </div>
         <div className="carousel-inner">
-          {/* TODO - Create mobile versions of these images and use a picture element to choose */}
-          {slides.map(({ slide, id }) => (
-            <div key={id} className={cx("carousel-item", id === 0 && "active")}>
-              <img src={slide} className="d-block w-100" alt="..." />
-            </div>
-          ))}
+          {carouselImages.map(
+            ({ id, alt, mobileImage, desktopImage, extraWideImage }) => (
+              <div
+                key={id}
+                className={cx("carousel-item", id === 0 && "active")}
+              >
+                <picture>
+                  <source srcSet={extraWideImage} media="(min-width: 1200px" />
+                  <source srcSet={desktopImage} media="(min-width: 600px" />
+                  <img
+                    src={mobileImage}
+                    className="w-100 img-fluid mx-auto"
+                    alt={alt}
+                  />
+                </picture>
+              </div>
+            )
+          )}
         </div>
         <button
           className="carousel-control-prev"
@@ -67,25 +91,40 @@ export default function Index() {
   );
 }
 
-const slides = [
+const carouselImages = [
   {
     id: 0,
-    slide: slide1,
+    alt: "Trees by the water",
+    mobileImage: mobileImage1,
+    desktopImage: desktopImage1,
+    extraWideImage: extraWideImage1,
   },
   {
     id: 1,
-    slide: slide2,
+    alt: "Young Burmese girl",
+    mobileImage: mobileImage2,
+    desktopImage: desktopImage2,
+    extraWideImage: extraWideImage2,
   },
   {
     id: 2,
-    slide: slide3,
+    alt: "Women and girls in the street",
+    mobileImage: mobileImage3,
+    desktopImage: desktopImage3,
+    extraWideImage: extraWideImage3,
   },
   {
     id: 3,
-    slide: slide4,
+    alt: "Young monks",
+    mobileImage: mobileImage4,
+    desktopImage: desktopImage4,
+    extraWideImage: extraWideImage4,
   },
   {
     id: 4,
-    slide: slide5,
+    alt: "Cuban street scene",
+    mobileImage: mobileImage5,
+    desktopImage: desktopImage5,
+    extraWideImage: extraWideImage5,
   },
 ];
