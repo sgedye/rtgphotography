466fd9adb81dcbab0fe6c5194ba7422e4521ac54
Shaun Gedye <sgedye88@yahoo.com.au>
2022-12-04 22:36:57 +0800

    Create app-offline page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ export const Footer = () => {
     >
       <div className="container text-white">
         <ul className="list-unstyled d-flex flex-column flex-sm-row align-items-center justify-content-between mb-0">
-          <li className="">© {new Date().getFullYear()}</li>
+          <li className="">&copy; {new Date().getFullYear()}</li>
           <li className="">
             <RtgLogo width={35} height={35} className="mb-2 mb-sm-0 me-2" />
             <span>RTG Photography</span>
